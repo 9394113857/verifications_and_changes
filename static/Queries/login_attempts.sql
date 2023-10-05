6584de1852d2d5b420f0dce23b81dbab1346062a
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2023-10-05 08:22:50 +0530

    Added requirements.txt and SQL queries with commands.txt file

--- static/Queries/login_attempts.sql
+++ static/Queries/login_attempts.sql
@@ -1,13 +1,16 @@
-SELECT id,
-       username,
-       timestamp,
-       is_blocked
-  FROM login_attempts;
+-- This query selects specific columns (id, username, timestamp, is_blocked) from the 'login_attempts' table.
+-- It retrieves information about login attempts, including the user's ID, username, timestamp, and whether the attempt is blocked or not.
+SELECT id, username, timestamp, is_blocked
+FROM login_attempts;
 
+-- This query selects specific columns (id, username, is_blocked) from the 'login_attempts' table.
+-- It retrieves information about the latest login attempt by ordering the rows by 'id' in descending order (latest first) and limiting the result to only one row.
 SELECT id, username, is_blocked
 FROM login_attempts
 ORDER BY id DESC
 LIMIT 1;
 
---
-DELETE FROM login_attempts
+-- This query deletes records from the 'login_attempts' table.
+-- It removes all records in the table without specifying any condition, which means it will delete all the login attempt records.
+DELETE FROM login_attempts;
+
--- static/Queries/login_history.sql
+++ static/Queries/login_history.sql
@@ -1,18 +1,16 @@
--- This query will retrieve the last entry from the login_history table based on the id column in descending order.
+-- This query selects the most recent login record from the login_history table.
+-- It retrieves the ID, user_id, login_timestamp, and logout_timestamp columns.
+-- The records are ordered in descending order by the 'id' column, and only the top record is returned.
 SELECT id, user_id, login_timestamp, logout_timestamp
 FROM login_history
 ORDER BY id DESC
 LIMIT 1;
 
---
-SELECT id,
-       user_id,
-       login_timestamp,
-       logout_timestamp
-  FROM login_history;
+-- This query selects all records from the login_history table.
+-- It retrieves the ID, user_id, login_timestamp, and logout_timestamp columns.
+SELECT id, user_id, login_timestamp, logout_timestamp
+FROM login_history;
 
---
-DELETE FROM login_history
-
-
-
+-- This query deletes all records from the login_history table.
+-- Be cautious when running this query, as it will remove all data from the table.
+DELETE FROM login_history;
